1d32e812e9cc2ba68826ead6301190592783ab51
casey <caseymcclenathan@Caseys-MacBook-Pro.local>
2021-10-04 21:16:55 -0500

    add user routes (create, login, verify)

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,10 @@ Rails.application.routes.draw do
     resources :dealers
     resources :lots
   end
+
+  post '/users', to: 'users#create'
+  post '/auth/login', to: 'authentication#login'
+  post '/auth/verify', to: 'authentication#verify'
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
