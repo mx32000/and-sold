710a96f7a3649ab7a0ed04dc7d3dafb709419b78
casey <caseymcclenathan@Caseys-MacBook-Pro.local>
2021-10-05 15:49:24 -0500

    api config file in services folder

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,18 @@
 import './App.css';
+import { Switch, Route, useHistory } from 'react-router-dom';
+import { useEffect, useState } from 'react';
 
 function App() {
+  const [currentUser, setCurrentUser] = useState(null);
+  const history = useHistory();
+
   return (
     <div className="App">
-      <h1>And...Sold!</h1>
+      <Switch>
+        <Route path="/">
+          <h1>hello</h1>
+        </Route>
+      </Switch>
     </div>
   );
 }
