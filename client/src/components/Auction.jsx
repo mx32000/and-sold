9f43e67ded714f6472ba8b9192c50bcfc81d24bf
casey <caseymcclenathan@Caseys-MacBook-Pro.local>
2021-10-06 17:09:37 -0500

    add see details page to auction cards

--- client/src/components/Auction.jsx
+++ client/src/components/Auction.jsx
@@ -1,10 +1,13 @@
+import { Link } from "react-router-dom";
+
 export default function Auction(props) {
-  const { title, status, organization } = props.auction;
+  const { title, status, organization, id } = props.auction;
   return(
     <div className="auction">
       <h2>{title}</h2>
       <p>Status: {status}</p>
       <p>Organization: {organization}</p>
+      <Link to={`/auctions/${id}`}><button>See details</button></Link>
     </div>
   )
 }
