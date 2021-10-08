19db8cbbb0191f121d87807ad3380d2b793c7948
casey <caseymcclenathan@Caseys-MacBook-Pro.local>
2021-10-07 23:28:26 -0500

    lots mapping in bidder details page

--- client/src/screens/Bidders/BidderDetail.jsx
+++ client/src/screens/Bidders/BidderDetail.jsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { Link } from "react-router-dom";
+import "../../assets/screens/Bidders/BidderDetail.css";
 
 export default function BidderDetail(props) {
   const [bidder, setBidder] = useState(null);
@@ -18,13 +19,21 @@ export default function BidderDetail(props) {
     bidder &&
     <div className="bidder-detail">
       <h1>Bidder Details</h1>
-      <div className="bidder-details">
-        <h2>Bidder #{bidder.number} - {bidder.name}</h2>
-        <p>Phone: {bidder.phone}</p>
-        <p>Address: {bidder.address}</p>
-        <p>Email: {bidder.email}</p>
-        <p>{bidder.tax_exempt ? "Tax-exempt" : "Not tax-exempt"}</p>
-        <Link to={`/auctions/${auction_id}/bidders/${id}/edit`}><button>Edit</button></Link>
+      <div className="bidder-details-container">
+        <div className="bidder-details">
+          <h2>Bidder #{bidder.number} - {bidder.name}</h2>
+          <p>Phone: {bidder.phone_number}</p>
+          <p>Address: {bidder.address}</p>
+          <p>Email: {bidder.email}</p>
+          <p>{bidder.tax_exempt ? "Tax-exempt" : "Not tax-exempt"}</p>
+          <Link to={`/auctions/${auction_id}/bidders/${id}/edit`}><button>Edit</button></Link>
+        </div>
+        <div className="bidder-lots">
+          <h4>Lots</h4>
+          {bidder.lots.map(lot => 
+            <p key={lot.id}>{lot.number} - {lot.description} - ${lot.price.toFixed(2)}</p>
+          )}
+        </div>
       </div>
     </div>
   )
