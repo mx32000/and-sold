d93385297feffddecb845a6819688ea7a3784775
casey <caseymcclenathan@Caseys-MacBook-Pro.local>
2021-10-10 16:44:39 -0500

    bidder create styling

--- client/src/screens/Bidders/BidderCreate.jsx
+++ client/src/screens/Bidders/BidderCreate.jsx
@@ -1,4 +1,6 @@
-import { useState } from "react"
+import { useState } from "react";
+import Button from "../../components/Button";
+import "../../assets/screens/Bidders/BidderCreate.css";
 
 export default function BidderCreate(props) {
   const [form, setForm] = useState({
@@ -25,54 +27,56 @@ export default function BidderCreate(props) {
 
   return(
     <div className="bidder-create">
-      <h1>Register bidder</h1>
       <form onSubmit={handleSubmit}>
-        <div className="col1 details">
-          <label htmlFor="name">Name</label>
-          <input
-            type="text"
-            name="name"
-            id="name"
-            placeholder="Rocky Smith"
-            value={name}
-            onChange={handleChange}
-          />
-          <label htmlFor="phone_number">Phone number</label>
-          <input
-            type="text"
-            name="phone_number"
-            id="phone_number"
-            placeholder="555-555-5555"
-            value={phone_number}
-            onChange={handleChange}
-          />
-          <label htmlFor="address">Address</label>
-          <input
-            type="text"
-            name="address"
-            id="address"
-            value={address}
-            placeholder="123 Main St, Townsville, IA 52404"
-            onChange={handleChange}
-          />
-          <label htmlFor="email">Email address</label>
-          <input
-            type="text"
-            name="email"
-            id="email"
-            value={email}
-            placeholder="rocky.smith@email.com"
-            onChange={handleChange}
-          />
+        <h1>Register bidder</h1>
+        <div className="bidder-create-container">
+          <div className="col1 details">
+            <label htmlFor="name">Name</label>
+            <input
+              type="text"
+              name="name"
+              id="name"
+              placeholder="Rocky Smith"
+              value={name}
+              onChange={handleChange}
+            />
+            <label htmlFor="phone_number">Phone number</label>
+            <input
+              type="text"
+              name="phone_number"
+              id="phone_number"
+              placeholder="555-555-5555"
+              value={phone_number}
+              onChange={handleChange}
+            />
+            <label htmlFor="address">Address</label>
+            <input
+              type="text"
+              name="address"
+              id="address"
+              value={address}
+              placeholder="123 Main St, Townsville, IA 52404"
+              onChange={handleChange}
+            />
+            <label htmlFor="email">Email address</label>
+            <input
+              type="text"
+              name="email"
+              id="email"
+              value={email}
+              placeholder="rocky.smith@email.com"
+              onChange={handleChange}
+            />
+          </div>
+          <div className="col2 details">
+            <label htmlFor="tax_exempt">Tax-exempt?</label>
+            <select name="tax_exempt" id="tax_exempt" value={tax_exempt} onChange={handleChange}>
+              <option value={true}>Yes</option>
+              <option value={false}>No</option>
+            </select>
+          </div>
         </div>
-        <div className="col2 details">
-          <label htmlFor="tax_exempt">Tax-exempt?</label>
-          <select name="tax_exempt" id="tax_exempt" value={tax_exempt} onChange={handleChange}>
-            <option value={true}>Yes</option>
-            <option value={false}>No</option>
-          </select>
-        </div>
-        <button type="submit">Register</button>
+        <Button text="Register" />
       </form>
     </div>
   )
